80320513f676ea8828f7d8e9e0134f8fa5bf7485
Boe92 <boemimarco@yahoo.it>
2016-10-18 09:53:57 +0200

    a

--- Autenticazione/src/com/autenticazione/boemi/Autentica.java
+++ Autenticazione/src/com/autenticazione/boemi/Autentica.java
@@ -1,11 +1,14 @@
 package com.autenticazione.boemi;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Autentica
@@ -27,10 +30,23 @@ public class Autentica extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String user = request.getParameter("user");
 		String pwd = request.getParameter("pwd");
 		
 		if(pwd.equals("123")) {
-			response.sendRedirect("benvenuto.html");
+			HttpSession session = request.getSession();
+			session.setAttribute("user", user);
+			
+			String output =
+					"<html>"+
+						"<body>"+
+							session.getAttribute("user")+
+						"</body>"+
+					"<html>";
+			
+			out.print(output);
+			//response.sendRedirect("benvenuto.html");
 		}else {
 			response.sendRedirect("login.html");
 		}
