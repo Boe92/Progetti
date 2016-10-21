30de10ab01560f3606045d812dfdf7c7e4ea15bf
Boe92 <boemimarco@yahoo.it>
2016-10-21 17:17:42 +0200

    aaaaaa

--- ProgettoAzienda/src/com/alfasoft/entita/Admin.java
+++ ProgettoAzienda/src/com/alfasoft/entita/Admin.java
@@ -4,5 +4,24 @@ import javax.persistence.Entity;
 
 @Entity
 public class Admin extends Utente{
+	private String livelloAccesso;
+	
+	public Admin() {
+		super();
+	}
+	
+	public Admin(String livelloAccesso) {
+		super();
+		this.livelloAccesso = livelloAccesso;
+	}
+
+	public String getLivelloAccesso() {
+		return livelloAccesso;
+	}
+
+	public void setLivelloAccesso(String livelloAccesso) {
+		this.livelloAccesso = livelloAccesso;
+	}
+	
 	
 }
